7d2e582b9dc0e5d39e9c43edff2502088f6958e1
Harry Nunyuie <selormharry@gmail.com>
2026-08-10 14:58:45 -0700

    Uncomment Terraform resources for S3 backend configuration

--- main.tf
+++ main.tf
@@ -1,36 +1,36 @@
-# resource "aws_s3_bucket" "remote_backend_bucket" {
-#   bucket = var.bucket_name
+resource "aws_s3_bucket" "remote_backend_bucket" {
+  bucket = var.bucket_name
 
-#   tags = {
-#     Name        = var.bucket_name
-#     Environment = var.env
-#   }
-# }
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env
+  }
+}
 
 
-# provider "aws" {
-#   region  = var.region
-# }
-# resource "aws_s3_bucket_versioning" "remote_backend_bucket_versioning" {
-#   bucket = aws_s3_bucket.remote_backend_bucket.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+provider "aws" {
+  region  = var.region
+}
+resource "aws_s3_bucket_versioning" "remote_backend_bucket_versioning" {
+  bucket = aws_s3_bucket.remote_backend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "remote_backend_bucket_encryption" {
-#   bucket = aws_s3_bucket.remote_backend_bucket.id
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "remote_backend_bucket_encryption" {
+  bucket = aws_s3_bucket.remote_backend_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "remote_backend_block" {
-#   bucket                  = aws_s3_bucket.remote_backend_bucket.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "remote_backend_block" {
+  bucket                  = aws_s3_bucket.remote_backend_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
