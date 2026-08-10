9673df9e25aa35e359553760507f82bc93e411d3
Harry Nunyuie <selormharry@gmail.com>
2026-08-10 14:45:30 -0700

    Refactor Terraform configuration for S3 backend: update variable formatting and adjust provider version constraints

--- main.tf
+++ main.tf
@@ -1,32 +1,36 @@
-resource "aws_s3_bucket" "remote_backend_bucket" {
-  bucket = "${var.bucket_name}"
- 
-  tags = {
-    Name        = "Remote Backend Bucket"
-    Environment = "${var.env}"
-  }
-}
+# resource "aws_s3_bucket" "remote_backend_bucket" {
+#   bucket = var.bucket_name
 
-resource "aws_s3_bucket_versioning" "remote_backend_bucket_versioning" {
-  bucket = "${aws_s3_bucket.remote_backend_bucket.id}"
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   tags = {
+#     Name        = var.bucket_name
+#     Environment = var.env
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "remote_backend_bucket_encryption" {
-  bucket = "${aws_s3_bucket.remote_backend_bucket.id}"
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
 
-resource "aws_s3_bucket_public_access_block" "remote_backend_block" {
-  bucket = "${aws_s3_bucket.remote_backend_bucket.id}"
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+# provider "aws" {
+#   region  = var.region
+# }
+# resource "aws_s3_bucket_versioning" "remote_backend_bucket_versioning" {
+#   bucket = aws_s3_bucket.remote_backend_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "remote_backend_bucket_encryption" {
+#   bucket = aws_s3_bucket.remote_backend_bucket.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+# resource "aws_s3_bucket_public_access_block" "remote_backend_block" {
+#   bucket                  = aws_s3_bucket.remote_backend_bucket.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,18 @@
 variable "bucket_name" {
-    type        = string
-    description = "The name of the S3 bucket to create for the remote backend."
+  type        = string
+  description = "The name of the S3 bucket to create for the remote backend."
+  default     = "selorm-s3-remote-backend-bucket"
 }
 
 
 variable "env" {
-    type       = string
-    description = "The environment for which the dev S3 bucket is being created"
-    default    = "dev"
+  type        = string
+  description = "The environment for which the dev S3 bucket is being created"
+  default     = "dev"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region where the S3 bucket will be created."
+  default     = "ap-southeast-2"
 }
